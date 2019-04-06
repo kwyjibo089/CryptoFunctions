c24e79c8f7d63306502ddcef1d7e30292b076ec8
Roman Müller <roman.mueller@swisslife.ch>
2019-04-06 10:04:25 +0200

    added transaction model

--- PortfolioFunction/AddHttpTrigger.cs
+++ PortfolioFunction/AddHttpTrigger.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using PortfolioFunction.Models;
 
 namespace PortfolioFunction
 {
@@ -17,17 +18,16 @@ namespace PortfolioFunction
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            log.LogInformation($"C# HTTP trigger function processed a request: {requestBody}");                        
+            
+            var transaction = Transaction.FromJson(requestBody);
+            
+            if(string.IsNullOrWhiteSpace( transaction.Currency))
+                new BadRequestObjectResult("Please pass a currency in the request body");
+                       
+            return new OkResult();
         }
     }
 }
---
+++ PortfolioFunction/Models/Transaction.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Globalization;
+
+namespace PortfolioFunction.Models
+{
+
+    public partial class Transaction
+    {
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("amount")]
+        public long Amount { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("transactiontype")]
+        public string Transactiontype { get; set; }
+    }
+
+    public partial class Transaction
+    {
+        public static Transaction FromJson(string json) => JsonConvert.DeserializeObject<Transaction>(json, Models.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Transaction self) => JsonConvert.SerializeObject(self, Models.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
+
