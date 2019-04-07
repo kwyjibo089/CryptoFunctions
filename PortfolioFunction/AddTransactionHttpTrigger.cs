86df025d1412c57f0ff6b2cb21af5ba1e482ae82
Roman Müller <roman.mueller@swisslife.ch>
2019-04-07 14:05:36 +0200

    adding configuration

--- PortfolioFunction/AddTransactionHttpTrigger.cs
+++ PortfolioFunction/AddTransactionHttpTrigger.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using PortfolioFunction.Models;
 using System.IO;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace PortfolioFunction
 {
@@ -13,8 +15,8 @@ namespace PortfolioFunction
     {
         [FunctionName("AddTransactionHttpTrigger")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log, ExecutionContext context)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
@@ -24,6 +26,16 @@ namespace PortfolioFunction
 
             if (string.IsNullOrWhiteSpace(transaction.Currency))
                 return new BadRequestObjectResult("Please pass a currency in the request body");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var url = config["CoinMarketCap:URL"];
+
+            req.HttpContext.Response.Headers.Add("API-URL", url);
 
             return new OkResult();
         }
