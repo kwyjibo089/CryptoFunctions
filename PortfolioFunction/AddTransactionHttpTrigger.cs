5be92a4dad8215d219daeb076016b9be083a721f
Roman Müller <roman.mueller@swisslife.ch>
2019-04-07 13:09:28 +0200

    triggering CI/CD

--- PortfolioFunction/AddTransactionHttpTrigger.cs
+++ PortfolioFunction/AddTransactionHttpTrigger.cs
@@ -1,13 +1,11 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using PortfolioFunction.Models;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace PortfolioFunction
 {
@@ -20,13 +18,13 @@ namespace PortfolioFunction
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            log.LogInformation($"C# HTTP trigger function processed a request: {requestBody}");                        
-            
+            log.LogInformation($"C# HTTP trigger function processed a request: {requestBody}");
+
             var transaction = Transaction.FromJson(requestBody);
-            
-            if(string.IsNullOrWhiteSpace( transaction.Currency))
+
+            if (string.IsNullOrWhiteSpace(transaction.Currency))
                 new BadRequestObjectResult("Please pass a currency in the request body");
-                       
+
             return new OkResult();
         }
     }
